9db2fe6b0917f101346c2d681dcc0124b631142f
Nate River <river177169@proton.me>
2025-02-05 03:48:09 +0530

    generated bindings

---
+++ src/bindings.rs
@@ -0,0 +1,13 @@
+#![allow(warnings)]
+//#![allow(unused_variables)]
+//#![allow(non_upper_case_globals,non_snake_case,non_camel_case_types)]
+
+pub mod sdk;
+pub mod ssl;
+pub mod arch;
+pub mod lwip;
+pub mod netif;
+pub mod driver;
+
+
+
